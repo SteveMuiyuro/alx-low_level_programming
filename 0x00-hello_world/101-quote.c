a051934f8d29e6c16cd0c0bc5692047d78c7a2d4
Steve <stevemuiyuro@gmail.com>
2022-09-08 20:20:54 +0300

    Revised code

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <stdlib.h>
 
 /**
   *main - Entry point
@@ -9,7 +10,7 @@
   */
 int main(void)
 {
-	fprintf(stderr, "and that piece of art is useful
-			\" - Dora Korpar, 2015-10-19\n");
+	write(STDOUT_FILENO, "and that piece of art is useful\"
+			- Dora Korpar, 2015-10-19\n" 59);
 	return (1);
 }
