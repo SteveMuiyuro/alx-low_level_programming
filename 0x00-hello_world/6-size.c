4512e79fc6ab5805df8b1f012d18f4ce7d60816f
Steve <stevemuiyuro@gmail.com>
2022-09-08 18:40:46 +0300

    Addded missing parenthesis on printf function

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -9,11 +9,17 @@
   */
 int main(void)
 {
-	printf("Size of a char: %d\n", sizeof(char);
-	printf("Size of an int: %d\n", sizeof(int);
-	printf("Size of a long int: %d\n", sizeof(long int);
-	printf("Size of a long long int: %d\n", sizeof(long long int);
-	printf("Size of a float: %d", sizeof(float);
+	char a;
+	int b;
+	long int c;
+	long long int d;
+	float e;
+
+	printf("Size of a char: %u\n", sizeof(a));
+	printf("Size of an int: %u\n", sizeof(b));
+	printf("Size of a long int: %u\n", sizeof(c));
+	printf("Size of a long long int: %u\n", sizeof(d));
+	printf("Size of a float: %u\n", sizeof(e));
 	return (0);
 }
 
