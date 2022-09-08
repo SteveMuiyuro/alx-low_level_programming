0614206a1a3a7df078d7851b74e519d77405f24a
Steve <stevemuiyuro@gmail.com>
2022-09-08 17:18:18 +0300

    Revised program

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -6,7 +6,7 @@
   *put() prints a string with a new line
   */
 int main(void)
-{	
-	puts(""Programming is like building a multilingual puzzle");
+{
+	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
 }
