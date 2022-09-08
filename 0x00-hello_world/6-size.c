6ee92b0589c0f36c198772d7bcd1b359e270dac8
Steve <stevemuiyuro@gmail.com>
2022-09-08 18:18:10 +0300

    Added a p;rogram that prints the size of various types on the computer it is compiled and run on

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+  *main - Entry point
+  *
+  *printf() function prints size of different data types
+  *
+  *Return: Always 0 (Success)
+  */
+int main(void)
+{
+	printf("Size of a char: %d\n", sizeof(char);
+	printf("Size of an int: %d\n", sizeof(int);
+	printf("Size of a long int: %d\n", sizeof(long int);
+	printf("Size of a long long int: %d\n", sizeof(long long int);
+	printf("Size of a float: %d", sizeof(float);
+	return (0);
+}
+
--- 0x00-hello_world/README.md
+++ 0x00-hello_world/README.md
@@ -4,3 +4,4 @@ This script generates the assembly code of a C code and save it in an output fil
 This script compiles a C file and creates an executable named cisfun
 This C program prints exactly "Programming is like building a multilingual puzzle, followed by a new line.
 This program prints exactly with proper grammar, but the outcome is a piece of art,
+This program prints the size of various types on the computer it is compiled and run on
