bf942c4ba1ac10df8603e4eb44359a8ccd121c49
Steve <stevemuiyuro@gmail.com>
2022-09-10 02:53:41 +0300

    Revised code

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -8,18 +8,25 @@
 int main(void)
 {
 	int i;
+	int j;
 
-	for (i = 0; i < 100; i++)
+	for (i = 0; i < 9; i++)
 
-		if (i != i)
+	{
+		for (j = i + 1; j < 10; j++)
+
 		{
-			putchar((i / 10) + '0');
 			putchar((i % 10) + '0');
-		}
-		if (i == 99)
+			putchar((j % 10) + '0');
+
+		if (i == 8 && j == 9)
 			continue;
+
 		putchar(',');
 		putchar(' ');
+
+		}
+	}
 
 	putchar('\n');
 	return (0);
