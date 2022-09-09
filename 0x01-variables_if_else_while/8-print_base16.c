857b1e6f224a04e3b398e3f357f1944bd20d2ef3
Steve <stevemuiyuro@gmail.com>
2022-09-10 01:57:42 +0300

    Revise code

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -10,7 +10,7 @@ int main(void)
 	int i;
 	int alpha;
 
-	for (i = 0; i < 16; i++)
+	for (i = 0; i < 10; i++)
 		putchar((i % 10) + '0');
 	for (alpha = 'a'; alpha <= 'f'; alpha++)
 		putchar(alpha);
