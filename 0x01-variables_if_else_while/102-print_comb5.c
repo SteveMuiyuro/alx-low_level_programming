236ec9109cf33d0d7e452c678775e5e8d9ff3b23
Steve <stevemuiyuro@gmail.com>
2022-09-10 03:52:42 +0300

    Revised code

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -10,11 +10,11 @@ int main(void)
 	int i;
 	int j;
 
-	for(i = 0; i < 98; i++)
+	for (i = 0; i <= 98; i++)
 
 	{
 
-		for (j = i + 1; j < 99; j++)
+		for (j = i + 1; j <= 99; j++)
 		{
 
 			putchar((i / 10) + '0');
