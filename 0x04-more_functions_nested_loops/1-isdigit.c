567eaf18cad5c910193292a5db7234bd47b672b4
Steve <stevemuiyuro@gmail.com>
2022-09-15 17:03:12 +0300

    Revise code

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,11 @@
+#include "main.h"
+/**
+  *_isdigit - This function checks for a digit (0 through 9).
+  *@c: The vlue being evaluated
+  *Return: 1 otherwise 0
+  */
+int _isdigit(int c)
+{
+	return (c >= 0 && c <= 9);
+
+}
--- 0x04-more_functions_nested_loops/README.md
+++ 0x04-more_functions_nested_loops/README.md
@@ -1,1 +1,2 @@
 This function checks for uppercase characters
+This function that checks for a digit (0 through 9).
