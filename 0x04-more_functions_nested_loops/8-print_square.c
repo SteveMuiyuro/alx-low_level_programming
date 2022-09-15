c13f261cca31ad2dda2c1fa995c51ddf122badeb
Steve <stevemuiyuro@gmail.com>
2022-09-16 00:29:36 +0300

    Added  function that prints a square, followed by a new line.

---
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,24 @@
+#include "main.h"
+/**
+  *print_square - This function prints a square followed by a newline
+  *@size: squre dimensions
+  */
+void print_square(int size)
+{
+	int i;
+	int j;
+
+	if (size <= 0)
+		_putchar('\n');
+	else
+	{
+		for (i = 0; i < size; i++)
+		{
+			for (j = 0; j < size; i++)
+				_putchar('#');
+			_putchar('\n');
+		}
+		_putchar('\n');
+	}
+
+{
--- 0x04-more_functions_nested_loops/README.md
+++ 0x04-more_functions_nested_loops/README.md
@@ -6,3 +6,4 @@ This function that prints the numbers, from 0 to 9, followed by a new line.
 This function that prints 10 times the numbers, from 0 to 14, followed by a new line.
 This function draws a straight line in the terminal.
 This function draws a diagonal line on the terminal.
+This function that prints a square, followed by a new line.
