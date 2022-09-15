bc6e59c43ee2f7388baa70b526e34fec1002eaf3
Steve <stevemuiyuro@gmail.com>
2022-09-15 17:16:55 +0300

    Revise code

--- 0x04-more_functions_nested_loops/2-mul.c
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -2,12 +2,10 @@
 /**
   *mul - Function multiplies two intergers
   *@a: is the first value being evaluated
-  *@b: is the econd value being evaluated
+  *@b: is the second value being evaluated
   *Return: Always 0
   */
 int mul(int a, int b)
 {
-	result = a * b;
-
-return (result);
+	return (a * b);
 }
