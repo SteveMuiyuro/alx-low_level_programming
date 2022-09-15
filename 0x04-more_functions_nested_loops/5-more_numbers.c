b99b1e3702d08013e3fabebaabe443fd8a68861c
Steve <stevemuiyuro@gmail.com>
2022-09-15 18:47:40 +0300

    Revised code

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -11,15 +11,12 @@ void more_numbers(void)
 	{
 		for (j = 0; j < 15; j++)
 		{
-			if (j > 9)
+			if (j >= 10)
 				_putchar((i / 10) + '0');
 
 			_putchar((j % 10) + '0');
 		}
-
 		_putchar('\n');
 	}
-
-
 }
 
