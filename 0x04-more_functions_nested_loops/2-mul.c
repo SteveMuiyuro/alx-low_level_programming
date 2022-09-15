13022238715ac2243935f24538dcabcd2d2edbd9
Steve <stevemuiyuro@gmail.com>
2022-09-15 17:12:59 +0300

    Added a function that multiplies two intergers

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+  *mul - Function multiplies two intergers
+  *@a: is the first value being evaluated
+  *@b: is the econd value being evaluated
+  *Return: Always 0
+  */
+int mul(int a, int b)
+{
+	result = a * b;
+
+return (result);
+}
--- 0x04-more_functions_nested_loops/README.md
+++ 0x04-more_functions_nested_loops/README.md
@@ -1,2 +1,3 @@
 This function checks for uppercase characters
 This function that checks for a digit (0 through 9).
+This function multplies to intergers
