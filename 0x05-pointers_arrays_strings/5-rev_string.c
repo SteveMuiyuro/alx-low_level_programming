e85a3b41bc4e856798b42bed06d989604e877060
Steve <stevemuiyuro@gmail.com>
2022-09-19 19:13:01 +0300

    Revised code

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -8,15 +8,17 @@ void rev_string(char *s)
 	int i, j, k;
 	char x;
 
-	i = j = 0;
+	i = 0;
+	j = 0;
 
-	while (s[i] != '\0';)
+	while (s[i] != '\0')
 		i++;
 
 	j = i - 1;
 
 	for (k = 0; k < i / 2; k++)
 		x = s[k];
+		s[k] = s[j];
 		s[j] = x;
 		j -= 1;
 
