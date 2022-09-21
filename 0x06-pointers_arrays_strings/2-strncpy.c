2479e87c31f7b6a54b0065da977e0c528c061751
Steve <stevemuiyuro@gmail.com>
2022-09-21 18:31:30 +0300

    Revised code

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -1,4 +1,5 @@
 #include "main.h"
+
 /**
   *_strncpy - This function copies a string
   *@dest: argument to the destination string
@@ -13,8 +14,10 @@ char *_strncpy(char *dest, char *src, int n)
 	i = 0;
 
 	while (src[i] != '\0' && i < n)
+	{
 		dest[i]	= src[i];
 		i++;
+	}
 	while (i < n)
 		dest[i++] = '\0';
 	return (dest);
