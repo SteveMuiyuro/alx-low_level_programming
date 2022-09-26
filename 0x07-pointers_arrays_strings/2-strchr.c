05aacb066ab911059f8642fce0c935ca89d34eb5
Steve <stevemuiyuro@gmail.com>
2022-09-26 15:00:39 +0300

    Added a function that locates a character in a string

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,32 @@
+#include "main.h"
+/**
+  *_strchr - The function locates a character in a string
+  *@s: is a string pointr argument
+  *@c: is a character
+  *Return: a pointer to the first occurrence of the
+  *character c in the string s, or NULL if the character
+  *is not found
+ */
+char *_strchr(char *s, char c)
+{
+	int i;
+	int j;
+	int count;
+
+	i = 0;
+	j = 0;
+	count = 0;
+
+	while (s[i] != '\0' && s[i] != ' ')
+	{
+		while (c[j] != '\0')
+		{
+			if (s[i] == c[j])
+				count++;
+		j++;
+
+		}
+
+		i++;
+	return (count);
+}
--- 0x07-pointers_arrays_strings/README.md
+++ 0x07-pointers_arrays_strings/README.md
@@ -1,2 +1,3 @@
 char *_memset(char *s, char b, unsigned int n) is a function that fills memory with a constant byte
 char *_memcpy(char *dest, char *src, unsigned int n) is a function that copies memory area
+char *_strchr(char *s, char c) is a function that locates a character in a string
