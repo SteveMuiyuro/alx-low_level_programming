a2580f8f61ac846671d22347aa603e2ceaf42b7a
Steve <stevemuiyuro@gmail.com>
2022-09-26 15:44:06 +0300

    Added a function that searches a string for any of a set of bytes.

---
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -0,0 +1,25 @@
+#include "main.h"
+/**
+  *_strpbrk - the function searches a string for any of a set of bytes.
+  *@s: first string pointer argument
+  *@accept: second string pointer argument
+  *Return: a pointer to the byte in s that matches
+  *one of the bytes in accept, or NULL if no such byte is found.
+  */
+char *_strpbrk(char *s, char *accept)
+{
+	int j;
+
+	while (*s)
+	{
+		j = 0;
+
+		while (*(accept + j) != '\0')
+		{
+			if (*(accept + j) == *s)
+				return (s);
+			j++;
+		}
+		s++;
+	}
+	return (0);
--- 0x07-pointers_arrays_strings/README.md
+++ 0x07-pointers_arrays_strings/README.md
@@ -2,3 +2,4 @@ char *_memset(char *s, char b, unsigned int n) is a function that fills memory w
 char *_memcpy(char *dest, char *src, unsigned int n) is a function that copies memory area
 char *_strchr(char *s, char c) is a function that locates a character in a string
 unsigned int _strspn(char *s, char *accept) is a function that gets the length of a prefix substring
+char *_strpbrk(char *s, char *accept) is a function that searches a string for any of a set of bytes.
