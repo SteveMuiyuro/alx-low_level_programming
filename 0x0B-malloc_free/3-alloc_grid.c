5a51dd2cf9a979f3078307b0ad034a98ccfd2a5f
Steve <stevemuiyuro@gmail.com>
2022-10-05 19:24:12 +0300

    Add a function that returns a pointer to a 2 dimensional array of integers.

---
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -0,0 +1,36 @@
+#include "main.h"
+/**
+  *alloc_grid - a function that returns a pointer to a 2
+  *dimensional array of integers.
+  *@width: array width
+  *@height: array height
+  *Return: a pointer to a 2 dimensional array of integers.
+  */
+int **alloc_grid(int width, int height)
+{
+
+	int **a;
+	int i, j;
+
+	if (width <= 0 || height <= 0)
+		return (NULL);
+	a = (int **)malloc(sizeof(int *) * height);
+	if (a == NULL)
+		return (NULL);
+	for (i = 0; i < height; i++)
+	{
+		a[i] = (int *)malloc(sizeof(int) * width);
+		if (a[i] == NULL)
+		{
+			for (j = 0; j < i; j++)
+				free(a[j]);
+			free(a);
+			return (NULL);
+		}
+		for (j = 0; j < width; j++)
+		{
+			a[i][j] = 0;
+		}
+	}
+	return (a);
+}
--- 0x0B-malloc_free/README.md
+++ 0x0B-malloc_free/README.md
@@ -1,3 +1,4 @@
 char *create_array(unsigned int size, char c) is a function that creates an array of chars, and initializes it with a specific char.
 char *_strdup(char *str) is a function that returns a pointer to a newly allocated space in memory, which contains a copy of the string given as a parameter.
 char *str_concat(char *s1, char *s2) is a function that concatenates two strings.
+int **alloc_grid(int width, int height) is a function that returns a pointer to a 2 dimensional array of integers.
