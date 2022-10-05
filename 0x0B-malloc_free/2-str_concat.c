eaf1940b8af17ba0d345b5963d42628c25c53224
Steve <stevemuiyuro@gmail.com>
2022-10-05 19:13:53 +0300

    Add a function that concatenates two strings.

---
+++ 0x0B-malloc_free/2-str_concat.c
@@ -0,0 +1,40 @@
+#include "main.h"
+/**
+  *str_concat - a function that concatenates two strings.
+  *@s1: first array pointer
+  *@s2: second array pointer
+  *Return: pointer should point to a newly allocated
+  *space in memory which contains the contents of s1,
+  *followed by the contents of s2, and null terminated
+  */
+char *str_concat(char *s1, char *s2)
+{
+	{
+	unsigned int i, j, k, l;
+	char *a;
+
+	if (s1 == NULL)
+		i = 0;
+	else
+	{
+		for (i = 0; s1[i]; i++)
+			;
+	}
+	if (s2 == NULL)
+		j = 0;
+	else
+	{
+		for (j = 0; s2[j]; j++)
+			;
+	}
+	k = i + j + 1;
+	a = malloc(k * sizeof(char));
+	if (a == NULL)
+		return (NULL);
+	for (l = 0; l < i; l++)
+		a[l] = s1[l];
+	for (l = 0; l < j; l++)
+		a[l + i] = s2[l];
+	a[i + j] = '\0';
+	return (a);
+}
--- 0x0B-malloc_free/README.md
+++ 0x0B-malloc_free/README.md
@@ -1,2 +1,3 @@
 char *create_array(unsigned int size, char c) is a function that creates an array of chars, and initializes it with a specific char.
 char *_strdup(char *str) is a function that returns a pointer to a newly allocated space in memory, which contains a copy of the string given as a parameter.
+char *str_concat(char *s1, char *s2) is a function that concatenates two strings.
