8787e363a21d81d8a80ebf1443196fd089e596d4
Steve <stevemuiyuro@gmail.com>
2022-10-06 19:47:16 +0300

    Add the function return description

--- 0x0C-more_malloc_free/0-malloc_checked.c
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -2,6 +2,7 @@
 /**
   *malloc_checked - is a function that allocates memory using malloc.
   *@b : is an interger value argument
+  *Return:  a pointer to the allocated memory
   */
 void *malloc_checked(unsigned int b)
 {
