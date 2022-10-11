3e395c5939784c1f602f0c07598ba033654afb34
Steve <stevemuiyuro@gmail.com>
2022-10-11 14:16:54 +0300

    Revise Header

--- 0x0D-preprocessor/2-main.c
+++ 0x0D-preprocessor/2-main.c
@@ -1,3 +1,12 @@
-#include "main.h"
+#include <stdio.h>
+/**
+  * main - prints the file's name followed by a new line
+  *Return: Always 0 (Success)
+  */
+int main(void)
+{
 
-printf("%s\n", __FILE__);
+	printf("%s\n", __FILE__);
+
+	return (0);
+}
