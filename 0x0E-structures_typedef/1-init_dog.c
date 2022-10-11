c7aec47ffa70a03447e4d9707fcc819876f4d18e
Steve <stevemuiyuro@gmail.com>
2022-10-11 18:12:45 +0300

    Add a library header

--- 0x0E-structures_typedef/1-init_dog.c
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -1,5 +1,5 @@
 #include "dog.h"
-
+#include <stdlib.h>
 /**
  * init_dog - initializes a structure of type dog
  * @d: pointer to structure
@@ -10,7 +10,7 @@
  */
 void init_dog(struct dog *d, char *name, float age, char *owner)
 {
-	if (d)
+	if (d != NULL)
 	{
 		d->name = name;
 		d->age = age;
