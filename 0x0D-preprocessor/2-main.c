6bd8389672e8b54ee588fdffc7814e297f31effe
Steve <stevemuiyuro@gmail.com>
2022-10-11 14:38:26 +0300

    Revise description

--- 0x0D-preprocessor/2-main.c
+++ 0x0D-preprocessor/2-main.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 /**
-  * main - prints the file's name followed by a new line
+  *main - prints the file's name followed by a new line
   *Return: Always 0 (Success)
   */
 int main(void)
